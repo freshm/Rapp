eb97eb59e10c780b91fdc9440958f232b954a1bf
Marvin Ahlgrimm <admin@raspberrypi.(none)>
2013-10-18 18:33:05 +0200

    Maeh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def verify_admin
+    unless user_signed_in? && current_user.admin?
+      flash[:error] = "You have no power here!" 
+      redirect_to root_url
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017212952) do
+ActiveRecord::Schema.define(version: 20131018115708) do
 
   create_table "users", force: true do |t|
     t.string   "firstname"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20131017212952) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
