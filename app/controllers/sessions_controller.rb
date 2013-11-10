fd0df0da64a53d4ab0849541f50146dc9e856a7b
freshm <mahlgrimm@W150HRM.(none)>
2013-11-10 21:22:27 +0100

    Fixed Login and added Flat UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,18 @@ class SessionsController < Devise::SessionsController
 
 		object = User.find_by_email(resource.email)
 		
-		if object.confirmed?
-			self.resource = warden.authenticate!(auth_options)
-			set_flash_message(:notice, :signed_in) if is_navigational_format?
-			sign_in(resource_name, resource)
-			respond_with resource, :location => after_sign_in_path_for(resource)
+		if object != nil
+			if object.confirmed?
+				self.resource = warden.authenticate!(auth_options)
+				set_flash_message(:notice, :signed_in) if is_navigational_format?
+				sign_in(resource_name, resource)
+				respond_with resource, :location => after_sign_in_path_for(resource)
+			else
+				flash[:error] = "The admin has not confirmed you yet."
+				redirect_to root_path
+			end
 		else
-			flash[:error] = "The admin has not confirmed you yet."
+			flash[:error] = "Invalid email or password."
 			redirect_to root_path
 		end
 	end
